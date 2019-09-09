4f8271eba18c0c981ebca913018cf2a885beeb55
Daniel Bunis <Daniel.Bunis@ucsf.edu>
2019-09-08 23:09:51 -0700

    Removed names from default dittoColor output
    This fixes a bug where colors had not been shown.
    
    Names are now retrievable with `dittoColors(get.names = TRUE)`

--- R/dittoColors.R
+++ R/dittoColors.R
@@ -1,5 +1,6 @@
 #' Extracts the DittoSeq default colors
 #' @author Daniel Bunis
+#' @param get.names Logical, whether only the names of the default DittoSeq color panel should be returned instead
 #' @description Creates a colors vector of 24 colors in hexadecimal form,
 #' a modification of the protanope and deuteranope friendly colors from Wong, B. Nature Methods, 2011.
 #'
@@ -14,20 +15,25 @@
 #' 17-24 = 25\% lighter versions of colors 1-8
 #'
 #' @export
-#' @example
+#' @examples
 #' dittoColors()
-dittoColors <- function() {
-    colors <- c(  # DittoSeq-v0.2.10 Colors
-        "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
-        "#D55E00", "#CC79A7", "#666666", "#AD7700", "#1C91D4",
-        "#007756", "#D5C711", "#005685", "#A04700", "#B14380",
-        "#4D4D4D", "#FFBE2D", "#80C7EF", "#00F6B3", "#F4EB71",
-        "#06A5FF", "#FF8320", "#D99BBD", "#8C8C8C")
-    names(colors) <- c(
-        "orange", "skyBlue", "bluishGreen", "yellow", "blue",
-        "vermillion", "reddishPurple", "gray40", "orange-25%", "skyBlue-25%",
-        "bluishGreen-25%", "yellow-25%", "blue-25%", "vermillion-25%", "reddishPurple-25%",
-        "gray40-25%", "orange+25%", "skyBlue+25%", "bluishGreen+25%", "yellow+25%",
-        "blue+25%", "vermillion+25%", "reddishPurple+25%", "gray40+25%")
-    colors
+#'
+#' #To retrieve names:
+#' dittoColors(get.names = TRUE)
+dittoColors <- function(get.names = FALSE) {
+    if (get.names) {
+        return(c(
+            "orange", "skyBlue", "bluishGreen", "yellow", "blue",
+            "vermillion", "reddishPurple", "gray40", "orange-25%", "skyBlue-25%",
+            "bluishGreen-25%", "yellow-25%", "blue-25%", "vermillion-25%", "reddishPurple-25%",
+            "gray40-25%", "orange+25%", "skyBlue+25%", "bluishGreen+25%", "yellow+25%",
+            "blue+25%", "vermillion+25%", "reddishPurple+25%", "gray40+25%"))
+    } else {
+        return(c(  # DittoSeq-v0.2.10 Colors
+            "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
+            "#D55E00", "#CC79A7", "#666666", "#AD7700", "#1C91D4",
+            "#007756", "#D5C711", "#005685", "#A04700", "#B14380",
+            "#4D4D4D", "#FFBE2D", "#80C7EF", "#00F6B3", "#F4EB71",
+            "#06A5FF", "#FF8320", "#D99BBD", "#8C8C8C"))
+    }
 }
