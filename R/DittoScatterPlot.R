71df4e1360291247c205aaecb3a1a8e5d328de74
Daniel Bunis <Daniel.Bunis@ucsf.edu>
2019-10-02 10:55:51 -0700

    Updated dittoScatterPlot docs and default titling

--- R/DittoScatterPlot.R
+++ R/DittoScatterPlot.R
@@ -1,53 +1,93 @@
 #' Show RNAseq data overlayed on a scatter plot
 #' @import ggplot2
 #'
-#' @param x.var Variable for setting x-axis position of cells/samples.  Can be the name of a gene, meta-data, or "ident" for clusters of a Seurat object.  Alternatively, can be a numeric of length equal to the total number of cells/samples in object.
-#' @param y.var Variable for setting y-axis position of cells/samples.  Can be the name of a gene, meta-data, or "ident" for clusters of a Seurat object.  Alternatively, can be a numeric of length equal to the total number of cells/samples in object.
-#' @param color.var Variable for setting the color of cells/samples in the plot.  Can be the name of a gene or meta-data.  Alternatively, can be "ident" for clusters of a Seurat object.  Alternatively, can be a numeric of length equal to the total number of cells/samples in object.
-#' @param shape.var Variable for setting the shape of cells/samples in the plot.  Note: must be discrete.  Can be the name of a gene or meta-data.  Alternatively, can be "ident" for clusters of a Seurat object.  Alternatively, can be a numeric of length equal to the total number of cells/samples in object.
+#' @param x.var Single string or numeric vector which sets x-axis position of cells/samples.
+#' Note: must be continuous.
+#' Can be the name of a gene or metadata.
+#' Alternatively, can be a numeric vector of length equal to the total number of cells/samples in object.
+#' @param y.var Single string or numeric vector which sets y-axis position of cells/samples.
+#' Note: must be continuous.
+#' Can be the name of a gene or metadata.
+#' Alternatively, can be a numeric vector of length equal to the total number of cells/samples in object.
+#' @param color.var Single string or numeric vector which sets the color of cells/samples in the plot.
+#' Can be the name of a gene, metadata, or "ident" for clusters of a Seurat object.
+#' Alternatively, can be a numeric or string vector or a factor of length equal to the total number of cells/samples in object.
+#' @param shape.var Single string or numeric vector which sets the shape of cells/samples in the plot.
+#' Note: must be discrete.
+#' Can be the name of a metadata, or "ident" for clusters of a Seurat object.
+#' Alternatively, can be a string vector or a factor of length equal to the total number of cells/samples in object.
 #' @param object A Seurat, SingleCellExperiment, or \linkS4class{RNAseq} object to work with, OR the name of the object in "quotes".
 #' REQUIRED, unless '\code{DEFAULT <- "object"}' has been run.
 #' @param cells.use String vector of cells'/samples' names which should be included.
 #' Alternatively, a Logical vector, the same length as the number of cells in the object, which sets which cells to include.
 #' For the typically easier logical method, provide \code{USE} in \code{object@cell.names[USE]} OR \code{colnames(object)[USE]}).
-#' @param show.others TRUE/FALSE. TRUE by default, whether other cells should be shown in the background
-#' @param color.panel a list of colors to be used for when plotting a discrete var.
-#' @param colors indexes / order of colors from color.panel to use. USAGE= changing the order of how colors are linked to specific groups
-#' @param data.type.x For when plotting expression data, sets the data-type slot that will be obtained. See \link[dittoSeq]{gene}. DEFAULT = "normalized"
-#' @param data.type.y For when plotting expression data, sets the data-type slot that will be obtained. See \link[dittoSeq]{gene}. DEFAULT = "normalized"
-#' @param data.type.color For when plotting expression data, sets the data-type slot that will be obtained. See \link[dittoSeq]{gene}. DEFAULT = "normalized"
-#' @param do.hover TRUE/FALSE. Default = FALSE.  If set to true: object will be converted to a ggplotly object so that data about individual points will be displayed when you hover your cursor over them.  'hover.data' argument is used to determine what data to use.  NOTE: incompatible with lettering (due to a ggplotly incompatibility). Setting do.hover to TRUE will override a do.letter=TRUE or NA.
-#' @param hover.data list of variable names, c("meta1","gene1","meta2","gene2"). determines what data to show on hover when do.hover is set to TRUE.
-#' @param hover.data.type For when plotting expression data, sets the data-type slot that will be obtained. See \link[dittoSeq]{gene}. DEFAULT = "normalized" For when plotting expression data: Should the data be "normalized" (data slot), "raw" (raw.data or counts slot), "scaled" (the scale.data slot of Seurat objects), "relative" (= pulls normalized data, then uses the scale() function to produce a relative-to-mean representation), or "normalized.to.max" (= pulls normalized data, then divides by the maximum value)? DEFAULT = "normalized"
+#' @param show.others Logical. TRUE by default, whether other cells should be shown in the background in light gray.
+#' @param color.panel String vector which sets the colors to draw from. \code{dittoColors()} by default, see \code{\link{dittoColors}} for contents.
+#' @param colors Integer vector, the indexes / order, of colors from color.panel to actually use
+#' @param data.type.x,data.type.y,data.type.color For when the axes or coloring are based on expression data, sets the data-type slot that will be obtained. See \code{\link{gene}} for options and details. DEFAULT = "normalized"
+#' @param do.hover Logical which controls whether the object will be converted to a plotly object so that data about individual points will be displayed when you hover your cursor over them.
+#' \code{hover.data} argument is used to determine what data to use.
+#' @param hover.data String vector of gene and metadata names, example: \code{c("meta1","gene1","meta2","gene2")} which determines what data to show on hover when \code{do.hover} is set to \code{TRUE}.
+#' @param hover.data.type For when showing expression data upon hover, sets the data-type slot that will be obtained. See \code{\link{gene}} for options and details. DEFAULT = "normalized"
 #' @param shape.panel Vector of integers corresponding to ggplot shapes which sets what shapes to use.
 #' When discrete groupings are supplied by \code{shape.var}, this sets the panel of shapes.
 #' When nothing is supplied to \code{shape.var}, only the first value is used.
-#' Default is a list of 6, \code{c(16,15,17,23,25,8)}, the first being a simple, solid, circle.
-#' Unfortunately, shapes can be hard to see, especially when points are on top of each other, and they are more slowly processed by the brain.
+#' Default is a set of 6, \code{c(16,15,17,23,25,8)}, the first being a simple, solid, circle.
+#'
+#' Note: Unfortunately, shapes can be hard to see when points are on top of each other & they are more slowly processed by the brain.
 #' For these reasons, even as a color blind person myself writing this code, I recommend use of colors for variables with many discrete values.
-#' @param size Number. Size of data points.  Default = 1.
-#' @param opacity Number between 0 and 1. Great for when you have MANY overlapping points, this sets how see-through the points should be; 1 = not at all; 0 = invisible. Default = 1.
-#' @param rename.color.groups Character vector containing new names for the identities of the color overlay.
-#' @param rename.shape.groups Character vector containing new names for the identities of the shape overlay.
-#' @param legend.show TRUE/FALSE. Whether the legend should be displayed. Default = TRUE.
-#' @param legend.color.title String which sets the title for the color legend. Default = \code{NULL} OR \code{var} when a shape legend will also be shown.
-#' @param legend.color.size Number representing the size to increase the plotting of color legend shapes to (for discrete variable plotting).
-#' Default = 5. *Enlarging the colors legend is incredibly helpful for making colors more distinguishable by color blind individuals.*
-#' @param legend.shape.title For adding a title for the colors legend.  Set to \code{NULL} to turn off
-#' @param legend.shape.size Override for legend size of the shape variable.
-#' @param min.color color for lowest values shown of the color overlay.  Default is \code{dittoColors()[4]}, a yellow.
-#' @param max.color color for highest values shown of the color overlay.  Default is \code{dittoColors()[5]}, a dark blue.
-#' @param min set the value associated with the minimum color.  All points with a lower value than this will get the same min.color.
-#' @param max set the value associated with the maximum color.  All points with a higher value than this will get the same max.color.  Note: if your legend is not plotting, it may be because min > max.
-#' @param legend.color.breaks Numeric vector. Sets the discrete values to show in the color-scale legend for continuous data.
-#' @param legend.color.breaks.labels String vector with same length as \code{breaks}. Renames the values displayed next to the color-scale.
-#' @param main String, sets the plot title. Default = "make" and if left as make, a title will be automatically generated.  To remove, set to \code{NULL}.
-#' @param sub String, sets the plot subtitle
-#' @param xlab String, label for y axes. Default = \code{x.var}. To remove, set to NULL.
-#' @param ylab String, label for y axes. Default = \code{y.var}. To remove, set to NULL.
+#' @param size Number which sets the size of data points. Default = 1.
+#' @param opacity Number between 0 and 1.
+#' Great for when you have MANY overlapping points, this sets how solid the points should be:
+#' 1 = not see-through at all. 0 = invisible. Default = 1.
+#' (In terms of typical ggplot variables, = alpha)
+#' @param rename.color.groups String vector containing new names for the identities of the color overlay groups.
+#' @param rename.shape.groups String vector containing new names for the identities of the shape overlay groups.
+#' @param legend.show Logical. Whether any legend should be displayed. Default = \code{TRUE}.
+#' @param legend.color.title,legend.shape.title Strings which sets the title for the color or shape legends. Default = \code{NULL} OR \code{color.var} when a shape legend will also be shown.
+#' @param legend.color.size,legend.shape.size Numbers representing the size to increase the plotting of color or shape legend icons to (for discrete variable plotting).
+#' Default = 5. *Enlarging the icons in the colors legend is incredibly helpful for making colors more distinguishable by color blind individuals.
+#' @param min.color color for lowest values of var/min.  Default = yellow
+#' @param max.color color for highest values of var/max.  Default = blue
+#' @param min Number which sets the value associated with the minimum color.
+#' @param max Number which sets the value associated with the maximum color.
+#' @param legend.color.breaks Numeric vector which sets the discrete values to show in the color-scale legend for continuous data.
+#' @param legend.color.breaks.labels String vector, with same length as \code{legend.breaks}, which renames what's displayed next to the tick marks of the color-scale.
+#' @param main String, sets the plot title.
+#' A default title is automatically generated if based on \code{color.var} and \code{shape.var} when either are provided.
+#' To remove, set to \code{NULL}.
+#' @param sub String, sets the plot subtitle.
+#' @param xlab,ylab Strings which set the labels for the axes. To remove, set to \code{NULL}.
 #' @param theme A ggplot theme which will be applied before dittoSeq adjustments. Default = \code{theme_bw()}. See \code{https://ggplot2.tidyverse.org/reference/ggtheme.html} for other options.
 #' @param data.out Whether just the plot should be output, or a list with the plot and Target_data and Others_data dataframes.  Note: plotly output is turned off in this setting, but hover.data is still calculated.
 #' @return Makes a plot where colored dots and/or shapes representing individual cells/samples are overlayed onto a scatterplot where x and y can be gene expression (or any numeric metadata) of those cells/samples.
+#' @details
+#' This function creates a dataframe with the X and Y coordinates determined by \code{x.var} and \code{y.var},
+#' It then adds data for how coloring should be set if a \code{color.var} is given & for how shapes should be set if a \code{shape.var} is given.
+#' The \code{data.type} inputs (\code{.x}, \code{.y}, \code{.color}) can be used to change what slot of expression data is used when the target data is gene expression data.
+#' If a set of cells to use is indicated with the \code{cells.use} input, then the dataframe is split into \code{Target_data} and \code{Others_data} based on subsetting by the target cells.
+#'
+#' Next, a scatter plot is then created using these dataframes.
+#' Non-target cells will be displayed in gray if \code{show.others=TRUE},
+#' and target cell data is displayed on top, colored and shaped based on the \code{color.var}- and \code{shape.var}-associated data.
+#'
+#' If \code{data.out=TRUE}, a list containing three slots is output: the plot (named 'p'), a data.table containing the underlying data for target cells (named 'Target_data'), and a data.table containing the underlying data for non-target cells (named 'Others_data').
+#'
+#' If \code{do.hover} is set to \code{TRUE}, the plot is coverted from ggplot to plotly & cell/sample information, determined by the \code{hover.data} input, is retrieved, added to the dataframe, and displayed
+#' upon hovering the cursor over the plot.
+#'
+#' Many characteristics of the plot can be adjusted using discrete inputs, including \code{size} and \code{opacity} to adjust the size and transparency of the data points.
+#' Colors can be adjusted with \code{color.panel} and/or \code{colors} for discrete data, or \code{min}, \code{max}, \code{min.color}, and \code{max.color} for continuous data.
+#' Shapes can be adjusted with \code{shape.panel}.
+#'
+#' Titles can be adjusted with \code{main}, \code{sub}, \code{xlab}, \code{ylab}, \code{legend.color.title}, and \code{legend.shape.title} arguments.
+#' Legends can also be adjusted in other ways, using variables that all start with "\code{legend.}" for easy tab-completion lookup.
+#'
+#' @seealso
+#' \code{\link{get.genes}} and \code{\link{get.metas}} to see what the \code{x.var}, \code{y.var}, \code{color.var}, \code{shape.var}, and \code{hover.data} options are.
+#'
+#' \code{\link{dittoDimPlot}} for making very similar data representations, but where dimensionality reduction (PCA, t-SNE, UMAP, etc.) dimensions are the scatterplot axes.
+#'
 #' @export
 #' @examples
 #'
@@ -80,19 +120,21 @@
 dittoScatterPlot <- function(
     x.var, y.var, color.var = NULL, shape.var = NULL,
     object = DEFAULT, cells.use = NULL, show.others = FALSE,
-    color.panel = dittoColors(), colors = seq_along(color.panel),
     size = 1, opacity = 1,
+    color.panel = dittoColors(), colors = seq_along(color.panel),
     data.type.x = "normalized", data.type.y = "normalized",
     data.type.color = "normalized", do.hover = FALSE, hover.data = NULL,
     hover.data.type = "normalized", shape.panel=c(16,15,17,23,25,8),
     rename.color.groups = NULL, rename.shape.groups = NULL,
     min.color = "#F0E442", max.color = "#0072B2", min = NULL, max = NULL,
-    xlab = x.var, ylab = y.var, main = NULL, sub = NULL, theme = theme_bw(),
+    xlab = x.var, ylab = y.var,
+    main = "make", sub = NULL, theme = theme_bw(),
     legend.show = TRUE,
     legend.color.title = color.var, legend.color.size = 5,
     legend.color.breaks = waiver(), legend.color.breaks.labels = waiver(),
     legend.shape.title = shape.var, legend.shape.size = 5,
     data.out = FALSE) {
+
     if (is.character(object)) {
         object <- eval(expr = parse(text = object))
     }
@@ -125,13 +167,18 @@ dittoScatterPlot <- function(
         do.shape <- TRUE
     }
     if (do.hover) {
-        hover.string <- .make_hover_strings_from_vars(hover.data, object, hover.data.type)
+        hover.string <- .make_hover_strings_from_vars(
+            hover.data, object, hover.data.type)
         aes.args$text = "hover.string"
     }
     Target_data <- dat[cells.use,]
     Others_data <- dat[!(all.cells %in% cells.use),]
 
-    ###Start building the plot###
+    # Set title
+    main <- .leave_default_or_null(main,
+        paste0(c(color.var, shape.var), collapse = " and "))
+
+    ### Start building the plot ###
     p <- ggplot() + ylab(ylab) + xlab(xlab) + ggtitle(main,sub) + theme
     if (do.shape) {
         p <- p +
@@ -158,8 +205,7 @@ dittoScatterPlot <- function(
             guides(color = guide_legend(override.aes = list(size=legend.color.size)))
         }
     }
-
-    ### Add data ###
+    # Add data
     if (show.others && nrow(Others_data)>1) {
         p <- p + geom_point(data = Others_data,
             aes_string(x = "X", y = "Y"), size=size, color = "gray90")
@@ -175,6 +221,7 @@ dittoScatterPlot <- function(
     if (!legend.show) {
         p <- .remove_legend(p)
     }
+
     ### RETURN the PLOT ###
     if (data.out) {
         return(list(plot = p, Target_data = Target_data, Others_data = Others_data))
