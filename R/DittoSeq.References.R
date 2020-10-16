2086365ca97672000c51764d897d8dbbbd9fbfd1
dtm2451 <Daniel.Bunis@ucsf.edu>
2020-10-16 14:34:11 -0700

    Update the `dittoSeq-package` documentation

--- R/DittoSeq.References.R
+++ R/DittoSeq.References.R
@@ -3,16 +3,17 @@
 #' @docType package
 #' @name dittoSeq
 #' @author Daniel Bunis
-#' @description This package was built to make the analysis and visualization of single-cell and bulk RNA-sequencing data accessible for both experienced and novice coders, and for color vision impaired individuals.
+#' @description This package was built to make the visualization of single-cell and bulk RNA-sequencing data pipeline-agnostic and accessible for both experienced and novice coders, and for color vision impaired individuals.
 #' @details Includes many plotting functions (\code{\link{dittoPlot}}, \code{\link{dittoDimPlot}}, \code{\link{dittoBarPlot}}, \code{\link{dittoHeatmap}}, ...),
-#' color adjustment functions (\code{\link{Simulate}}, \code{\link{Darken}}, \code{\link{Lighten}}),
-#' and helper funtions (\code{\link{meta}}, \code{\link{gene}}, \code{\link{isMeta}}, \code{\link{getMetas}}, ...) to aid in making sense of single cell or bulk RNA sequencing data.
-#' All included plotting functions produce a ggplot object (or pheatmap for dittoHeatmap) by default and can spit out a full plot with just a few arguments.
-#' Many additional arguments are available for customization to generate complex publication-ready figures.
+#' helper funtions (\code{\link{meta}}, \code{\link{gene}}, \code{\link{isMeta}}, \code{\link{getMetas}}, ...),
+#' and color adjustment functions (\code{\link{Simulate}}, \code{\link{Darken}}, \code{\link{Lighten}}),
+#' to aid in making sense of RNA sequencing data.
+#' All included plotting functions produce a ggplot object (or \code{\link[pheatmap]{pheatmap}} / \code{\link[ComplexHeatmap]{Heatmap}} for dittoHeatmap) by default and can spit out a full plot with just a few arguments.
+#' Many additional arguments are available for customization to generate complex, publication-ready figures.
 #'
 #' Default \code{\link{dittoColors}} are color blindness friendly and adapted from \href{https://www.nature.com/articles/nmeth.1618}{Wong B, "Points of view: Color blindness." Nature Methods, 2011.}
 #' 
-#' To report bugs, suggest new features, or ask for help, the best method is to create an issue on the github, \href{https://github.com/dtm2451/dittoSeq}{here}.
+#' To report bugs, suggest new features, or ask for help, the best method is to create an issue on the github, \href{https://github.com/dtm2451/dittoSeq}{here}, or the bioconductor support site (be sure to tag 'dittoSeq' so that I get a notification!), \href{https://support.bioconductor.org/}{here}
 NULL
 
 #' demuxlet.example
